9bea158cd1c0c94c1d9e51cf91583dc16fc1d6d6
Jeremy Nelson <jermnelson@gmail.com>
2019-04-23 16:47:55 -0600

    working blog

---
+++ README.md
@@ -0,0 +1,3 @@
+# Readme
+Format for Google drive photos:
+https://drive.google.com/uc?export=view&id=1z3ERNKTbMTOdNNEkf_JgnWzTO6VribWv
--- generate.py
+++ generate.py
@@ -6,7 +6,7 @@ import os
 from bs4 import BeautifulSoup
 import markdown
 
-with open("index.html") as fo:
+with open("index-template.html") as fo:
     index = BeautifulSoup(fo)
 
 postings = index.find("div", class_="postings")
@@ -17,15 +17,23 @@ for year in years:
     posts_walk = next(os.walk(os.path.abspath(f"posts/{year}")))
     posts = posts_walk[-1]
     for post in posts:
+        div_container = index.new_tag("div")
+        div_container['class'] = "blog-post"
         blog_ident = f"{year}/{post[0:5]}"
+        blog_label = f"{post[0:5]}-{year}"
         blog_date = index.new_tag("h2", id=blog_ident)
-        blog_date.string = f"{post[0:5]}-{year}"
-        postings.append(blog_date)
+        blog_date.string = blog_label
+        li_blog = index.new_tag("li")
+        li_blog_a = index.new_tag("a", href=f"#{blog_ident}")
+        li_blog_a.string = blog_label
+        li_blog.append(li_blog_a)
+        latest.append(li_blog)
+        div_container.append(blog_date)
         with open(os.path.abspath(f"posts/{year}/{post}")) as fo:
             post_html = markdown.markdown(fo.read())
             post_soup = BeautifulSoup(post_html)
-            for element in post_soup.find('body').children:
-                print(element)
-            #    postings.append(element)
-        postings.append(index.new_tag("hr"))
-#print(index)
+            body = post_soup.find('body')
+            div_container.append(body)
+        postings.append(div_container)
+with open("index.html", "w+") as fo:
+    fo.write(index.prettify())
